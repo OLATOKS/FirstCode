277ed70e8c406d56f47204be0b99f0f92423084d
OLATOKS <olajidetokunboh@gmail.com>
2024-04-09 12:16:17 +0100

    coded to bone structure

--- DoltNow.py.py
+++ DoltNow.py.py
@@ -1,31 +1,7 @@
 import random
-print("Good day welcome to pyprojects")
 print("what is your name?")
 name = input()
-print("welcome", name )
-password = 'olajidetokunboh'
-print("Do you have an account, if not sign up ")
-Accountanswer = input()
-if Accountanswer == 'yes':
-    print("welcome and please input your password below")
-    passwordinput = input()
-    if passwordinput != password:
-        while passwordinput!= password:
-            print("try again")
-            passwordinput = input()
-    if passwordinput == password:
-        print("ok")
-if Accountanswer == 'no':
-    print("we will need you to sign in, input your email and password")
-    email = input()
-    first_sign_in_password = input()
-    print("please input your password below")
-    passwordinput = input()
-    if passwordinput != first_sign_in_password:
-        while passwordinput != first_sign_in_password:
-            print("try again")
-            passwordinput = input()
-print("welcome, Mr", name)
+print("welcome" ,  name)
 print("please input the project you want to work on below", name)
 pyprojects = []
 newprojects = input()
@@ -50,13 +26,5 @@ if workans == 'yes':
     print("you will be given a random project to start with below!")
 randomprojects = random.choice(pyprojects)
 print("you are too work on", randomprojects,"now")   
-print("when done with project type in next!, if not type in no")
-nxtproject = input()
-if nxtproject == 'no':
-    print("complete the given project before moving onto the next")
-    while nxtproject == 'no':
-        print("are you done with the given project, yes or no \n if done type in next,\nif not type in no")
-        nxtproject = input()
-    if nxtproject == 'next':
-        pyprojects.remove(randomprojects)
+
 print(pyprojects)
