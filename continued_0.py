2cb77b7f84121fef9a67fe5001bfdb16c0c6738b
OLATOKS <olajidetokunboh@gmail.com>
2024-04-10 16:44:25 +0100

    corrected

--- continued_0.py
+++ continued_0.py
@@ -3,8 +3,8 @@ class Project:
     def __init__(self,Name):
         self.Name = Name
         self.TasksLists = []
-    def WhatAreTheTasks(self,Tasks):
-        return Tasks
+    def listTasks(self):
+        print(self.TasksLists)
 
     def AddMoreTask(self,Tasks):
         self.TasksLists.append(Tasks)
@@ -12,8 +12,8 @@ class Project:
         
     def RemoveTask(self,Tasks):
         self.TasksLists.remove(Tasks)
-print("enter the name of the new obeject")
-ProjectName = str(input())  
+#print("enter the name of the new obeject")
+#ProjectName = str(input())  
 ProjectName = Project("Tokunboh")
 ProjectName.AddMoreTask("go and eat , go and sleep , cleaning ")
-print(ProjectName.Name,"your projects are",ProjectName.WhatAreTheTasks(ProjectName.TasksLists))
+ProjectName.listTasks()
